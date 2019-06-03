ab849cb7ad09a58625392cb621f3dfcf9d584bed
unknown <neel12@somaiya.edu>
2019-06-03 12:38:09 +0530

    deviceModel previous_values fixed

--- src/kicadtoNgspice/DeviceModel.py
+++ src/kicadtoNgspice/DeviceModel.py
@@ -65,6 +65,8 @@ class DeviceModel(QtGui.QWidget):
         print("Reading Device model details from Schematic")
 
         for eachline in schematicInfo:
+            print("=========================================")
+            print(eachline)
             words = eachline.split()
             if eachline[0] == 'q':
                 print("Device Model Transistor: ", words[0])
@@ -82,7 +84,7 @@ class DeviceModel(QtGui.QWidget):
 
                 try:
                     for key in json_data["deviceModel"]:
-                        if key[0] == eachline[0] and key[1] == eachline[1]:
+                        if key[0] == words[0][0] and key[1:] == words[0][1:]:
                             # print "DEVICE MODEL MATCHING---",child.tag[0],\
                             # child.tag[1],eachline[0],eachline[1]
                             try:
@@ -90,8 +92,9 @@ class DeviceModel(QtGui.QWidget):
                                         json_data["deviceModel"][key][0]):
                                     self.entry_var[self.count].setText(
                                         json_data["deviceModel"][key][0])
-                                    path_name = json_data
-                                    ["deviceModel"][key][0]
+                                    path_name = (
+                                        json_data["deviceModel"][key][0]
+                                    )
                                 else:
                                     self.entry_var[self.count].setText("")
                             except BaseException:
@@ -147,14 +150,15 @@ class DeviceModel(QtGui.QWidget):
                 # global path_name
                 try:
                     for key in json_data["deviceModel"]:
-                        if key[0] == eachline[0] and key[1] == eachline[1]:
+                        if key[0] == words[0][0] and key[1:] == words[0][1:]:
                             # print "DEVICE MODEL MATCHING---",child.tag[0],\
                             # child.tag[1],eachline[0],eachline[1]
                             try:
                                 if os.path.exists(
                                         json_data["deviceModel"][key][0]):
-                                    path_name = json_data
-                                    ["deviceModel"][key][0]
+                                    path_name = (
+                                        json_data["deviceModel"][key][0]
+                                    )
                                     self.entry_var[self.count].setText(
                                         json_data["deviceModel"][key][0])
                                 else:
@@ -211,7 +215,7 @@ class DeviceModel(QtGui.QWidget):
                 # global path_name
                 try:
                     for key in json_data["deviceModel"]:
-                        if key[0] == eachline[0] and key[1] == eachline[1]:
+                        if key[0] == words[0][0] and key[1:] == words[0][1:]:
                             # print "DEVICE MODEL MATCHING---",child.tag[0],\
                             # child.tag[1],eachline[0],eachline[1]
                             try:
@@ -219,8 +223,9 @@ class DeviceModel(QtGui.QWidget):
                                         json_data["deviceModel"][key][0]):
                                     self.entry_var[self.count].setText(
                                         json_data["deviceModel"][key][0])
-                                    path_name = json_data
-                                    ["deviceModel"][key][0]
+                                    path_name = (
+                                        json_data["deviceModel"][key][0]
+                                    )
                                 else:
                                     self.entry_var[self.count].setText("")
                             except BaseException:
@@ -323,7 +328,7 @@ class DeviceModel(QtGui.QWidget):
                 # global path_name
                 try:
                     for key in json_data["deviceModel"]:
-                        if key[0] == eachline[0] and key[1] == eachline[1]:
+                        if key[0] == words[0][0] and key[1:] == words[0][1:]:
                             # print "DEVICE MODEL MATCHING---",child.tag[0],\
                             # child.tag[1],eachline[0],eachline[1]
                             while i <= end:
@@ -332,8 +337,9 @@ class DeviceModel(QtGui.QWidget):
                                 if (i - beg) == 0:
                                     if os.path.exists(
                                             json_data["deviceModel"][key][0]):
-                                        path_name = json_data
-                                        ["deviceModel"][key][0]
+                                        path_name = (
+                                            json_data["deviceModel"][key][0]
+                                        )
                                     else:
                                         self.entry_var[i].setText("")
                                 i = i + 1
@@ -404,6 +410,7 @@ class DeviceModel(QtGui.QWidget):
         self.widgetObjCount = iter_value
         print("self.widgetObjCount-----", self.widgetObjCount)
         self.libfile = path_value
+        print("PATH VALUE", path_value)
         # print "Selected Library File :",self.libfile
 
         # Setting Library to Text Edit Line
